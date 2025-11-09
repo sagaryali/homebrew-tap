dfddada714b2da37a41a26253d556b6996581246
sagaryali <119834808+sagaryali@users.noreply.github.com>
2025-11-09 00:24:09 -0500

    Implement caveats method in Adi class
    Added caveats method to provide user information.

--- adi.rb
+++ adi.rb
@@ -1,15 +1,30 @@
 class Adi < Formula
   desc "Personal CLI command for adi"
   homepage "https://github.com/sagaryali/adi-cli"
-url "https://github.com/sagaryali/adi-cli/archive/v1.0.0.tar.gz"
-sha256 "efc052a5dbe3f2cf1af718ff8ca7610992d5d5c7929ecca3592ca10a9eb8852b"
-version "1.0.0"
+  url "https://github.com/sagaryali/adi-cli/archive/v1.0.0.tar.gz"
+  sha256 "efc052a5dbe3f2cf1af718ff8ca7610992d5d5c7929ecca3592ca10a9eb8852b"
+  version "1.0.0"
 
-def install
-bin.install "adi"
-end
+  def install
+    bin.install "adi"
+  end
 
-test do
-system "#{bin}/adi"
-end
+  def caveats
+    <<~EOS
+    ___    ____  ____
+   /   |  / __ \/  _/
+  / /| | / / / // /  
+ / ___ |/ /_/ // /   
+/_/  |_/_____/___/   
+                     
+Hello! This is the adi command.
+More information coming soon...
+
+Type 'adi' anytime to see this message again!
+    EOS
+  end
+
+  test do
+    system "#{bin}/adi"
+  end
 end
