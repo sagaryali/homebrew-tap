d079471aa03447015e9b57fcb5abe34043890442
sagaryali <119834808+sagaryali@users.noreply.github.com>
2025-11-08 22:41:20 -0500

    Fix formatting of sha256 line in adi.rb

--- adi.rb
+++ adi.rb
@@ -2,7 +2,7 @@ class Adi < Formula
   desc "Personal CLI command for adi"
   homepage "https://github.com/sagaryali/adi-cli"
 url "https://github.com/sagaryali/adi-cli/archive/v1.0.0.tar.gz"
-sha256 "efc052a5dbe3f2cf1af718ff8ca7610992d5d5c7929ecca3592ca10a9eb8852b
+sha256 "efc052a5dbe3f2cf1af718ff8ca7610992d5d5c7929ecca3592ca10a9eb8852b"
 version "1.0.0"
 
 def install
